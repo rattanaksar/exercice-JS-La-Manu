9a3613ff171127838bb1d80df18018bb189f91d0
rattanaksar <81554110+rattanaksar@users.noreply.github.com>
2021-04-26 19:41:01 +0200

    Exo 4 Les conditions

---
+++ Exercice JS/scripts/main9.js
@@ -0,0 +1,23 @@
+let Note = prompt ("Mettez une note")
+if ((Note >= 0) && (Note < 3))
+{
+       alert("Nul")
+}
+else if((Note >= 4) && (Note < 6))
+{
+       alert("Moyen")
+}
+else if((Note >= 6) && (Note < 8))
+{
+       alert("Assez Bien")
+}
+else if((Note >= 8) && (Note = 9))
+{
+       alert("Bien")
+}
+else if(Note = 10)
+{
+       alert("Excellent")
+}
+
+
