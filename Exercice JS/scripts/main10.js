77d15fad6e3877f768445b1d53c21821872a4931
rattanaksar <81554110+rattanaksar@users.noreply.github.com>
2021-04-27 10:08:43 +0200

    Exercice 5 les conditions

---
+++ Exercice JS/scripts/main10.js
@@ -0,0 +1,24 @@
+const day = ""
+switch (day) {
+    case "7":
+        console.log ("Dimanche")
+        break
+    case "1":
+        console.log("Lundi")
+        break
+    case "2":
+        console.log("Mardi")
+        break
+    case "3":
+        console.log("Mercredi")
+        break
+    case "4":
+        console.log("Jeudi")
+        break
+    case "5":
+        console.log("Vendredi")
+        break
+    case "6":
+        console.log("Samedi")
+        break
+}
